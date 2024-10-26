d88f0589c279cfbf7d5e0dd97303f274b6c43610
solomon-winner <solyalew3@gmail.com>
2024-10-26 16:46:53 +0300

    ...song and testimony schema...

---
+++ src/models/songsModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const SongSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    link: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    img: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    albums: [{
+        type: String,
+    }],
+}, {
+    timestamps: true
+});
+
+const Song = mongoose.model("Song", SongSchema);
+
+export default Song;
