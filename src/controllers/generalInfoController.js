72ea0b9ce9b61231a9b949576c2d96c19bb3e0fb
solomon-winner <solyalew3@gmail.com>
2024-10-26 21:41:24 +0300

    ...general information controller...

---
+++ src/controllers/generalInfoController.js
@@ -0,0 +1,5 @@
+export const getGeneralInfo = async (req, res) => {};
+export const updateGeneralInfo = async (req, res) => {};
+export const deleteGeneralInfo = async (req, res) => {};
+export const addGeneralInfo = async (req, res) => {};
+export const verifyGeneralInfo = async (req, res) => {};
