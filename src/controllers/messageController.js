5749547adbc3ea7a480584d91beeb4a60fec1168
solomon-winner <solyalew3@gmail.com>
2024-10-26 21:43:10 +0300

    ...message controller...

---
+++ src/controllers/messageController.js
@@ -0,0 +1,5 @@
+export const addMessage = async (req, res) => {};
+export const getMessages = async (req, res) => {};
+export const getMessage = async (req, res) => {};
+export const removeMessage = async (req, res) => {};
+export const updateMessage = async (req, res) => {};
