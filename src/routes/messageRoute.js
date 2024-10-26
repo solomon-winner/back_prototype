9a7caca4b472724426b97f82d28622dedeab2447
solomon-winner <solyalew3@gmail.com>
2024-10-26 22:09:40 +0300

    ...message route...

---
+++ src/routes/messageRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { addMessage, updateMessage, removeMessage, getMessage, getMessages } from "../controllers/messageController";
+
+const router = express.Router();
+
+router.post("/", addMessage);
+router.put("/", updateMessage);
+router.get("/", getMessages);
+router.get("/:id", getMessage);
+router.delete("/", removeMessage);
+
+export default router;
