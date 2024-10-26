aa5bdcf43ffc66db3e742f367d0566594cb27ad9
solomon-winner <solyalew3@gmail.com>
2024-10-26 17:00:40 +0300

    ...banner cards schema...

---
+++ src/models/BannerCards.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, {
+    timestamps: true
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+
+export default Message;
