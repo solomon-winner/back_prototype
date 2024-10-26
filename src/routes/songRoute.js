e9cb2407e812977b383fe6849a943f0171a3a17f
solomon-winner <solyalew3@gmail.com>
2024-10-26 22:11:33 +0300

    ...song route...

---
+++ src/routes/songRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { addSong, updateSong, removeSong, getSong, getSongs } from "../controllers/songController";
+
+const router = express.Router();
+
+router.post("/", addSong);
+router.put("/", updateSong);
+router.get("/", getSongs);
+router.get("/:id", getSong);
+router.delete("/", removeSong);
+
+export default router;
