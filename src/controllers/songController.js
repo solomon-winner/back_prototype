d89a49734b9afe74fe947dba1e25c04267fc6938
solomon-winner <solyalew3@gmail.com>
2024-10-26 21:44:38 +0300

    ...creating song controller...

---
+++ src/controllers/songController.js
@@ -0,0 +1,5 @@
+export const addSong = async (req, res) => {};
+export const getSongs = async (req, res) => {};
+export const getSong = async (req, res) => {};
+export const removeSong = async (req, res) => {};
+export const updateSong = async (req, res) => {};
