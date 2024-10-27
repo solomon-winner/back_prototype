5e61e45679aba023483020fee339da4aa2f75f6d
solomon-winner <solyalew3@gmail.com>
2024-10-27 15:26:55 +0300

    ...

--- src/config/swaggerConfig.js
+++ src/config/swaggerConfig.js
@@ -5,7 +5,7 @@ const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "EUDR API",
+            title: "server prototype API",
             version:"1.0.0",
             description: "API documentation for EUDR",
         },
