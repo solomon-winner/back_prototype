e4dc7700ab33c2930c7e3a885a84ba3e0c32523e
solomon-winner <solyalew3@gmail.com>
2024-10-27 14:41:16 +0300

    ...

---
+++ src/dtos/bannerCards/bannerCardsDto.js
@@ -0,0 +1,9 @@
+export class bannerCardsdDTO {
+    constructor(_id, bannerInfo, bannerPic, aboutPic, aboutInfo, visitors, subscribers, bannerCards, createdAt, updatedAt ) {
+        this.id = _id;
+        this.title = bannerPic;
+        this.desdription = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
