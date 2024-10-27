dbd603bfbd0235b397579b201553cf9e4d84b940
solomon-winner <solyalew3@gmail.com>
2024-10-27 14:18:07 +0300

    ...

---
+++ src/dtos/general/generalForDashboardDto.js
@@ -0,0 +1,14 @@
+export class GeneralDTO {
+    constructor(_id, bannerInfo, bannerPic, aboutPic, aboutInfo, visitors, subscribers, bannerCards, createdAt, updatedAt ) {
+        this.id = _id;
+        this.bannerPic = bannerPic;
+        this.bannerInfo = bannerInfo;
+        this.aboutPic = aboutPic;
+        this.aboutInfo = aboutInfo
+        this.visitors = visitors;
+        this.subscribers = subscribers;
+        this.bannerCards = bannerCards
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
