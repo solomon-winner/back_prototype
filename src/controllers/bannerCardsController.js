7af05e9ba1688390eb1c8d095789992d956a34d5
solomon-winner <solyalew3@gmail.com>
2024-10-27 17:57:27 +0300

    ...banner cards...

--- src/controllers/bannerCardsController.js
+++ src/controllers/bannerCardsController.js
@@ -1,4 +1,18 @@
-export const addBannerCard = async (req, res) => {};
+import BannerCard from "../models/BannerCards.js";
+import BannerCardDTO from "../dtos/bannerCards/bannerCardsDto.js";
+
+export const addBannerCard = async (req, res) => {
+    try {
+        const { title, description} = req.body;
+        const bannerCard = await BannerCard.create({
+        title,
+        description,
+        });
+        return res.status(201).json({ bannerCard });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
 export const getBannerCards = async (req, res) => {};
 export const removeBannerCard = async (req, res) => {};
 export const updateBannerCard = async (req, res) => {};
