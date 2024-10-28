8535ae8e0b752fc80af3b824b62999242baaa486
solomon-winner <solyalew3@gmail.com>
2024-10-28 22:20:07 +0300

    ...

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -18,7 +18,32 @@ export const addMessage = async (req, res, next) => {
         next(error);
     }
 };
-export const getMessages = async (req, res, next) => {};
-export const getMessage = async (req, res, next) => {};
+export const getMessages = async (req, res, next) => {
+    try {
+        const messages = await Message.find({});
+        const messagesDtos = messages.map((message) => new MessageDTO(message));
+        return ResponseHelper.success(
+            res,
+            'Messages fetched successfully',
+            messagesDtos,
+        );
+    } catch (error) {
+        next(error);
+    }
+};
+export const getMessage = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const message = await Message.findById(id);
+        if (!message) {
+            return ResponseHelper.error(res, 'Message not found', [], 404);
+        }
+        return ResponseHelper.success(res, 'Message fetched successfully!', {
+            message: new MessageDTO(message),
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 export const removeMessage = async (req, res, next) => {};
 export const updateMessage = async (req, res, next) => {};
