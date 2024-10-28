1ab725f8d01259c053fcf08b3fd916767aefa858
solomon-winner <solyalew3@gmail.com>
2024-10-28 22:11:00 +0300

    ...message controller...

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,5 +1,24 @@
-export const addMessage = async (req, res) => {};
-export const getMessages = async (req, res) => {};
-export const getMessage = async (req, res) => {};
-export const removeMessage = async (req, res) => {};
-export const updateMessage = async (req, res) => {};
+import { MessageDTO } from "../dtos/message/messageDto.js";
+import Message from "../models/messageModel.js";
+
+export const addMessage = async (req, res, next) => {
+    try {
+        const { title, description } = req.body;
+        if (!title || !description) {
+        return ResponseHelper.error(res, 'Title and description are required', [], 400);
+        }
+        const newMessage = new Message({
+        title,
+        description,
+        });
+    
+        await newMessage.save();
+        return ResponseHelper.success(res, 'Message added Successfully', { message: new MessageDTO(newMessage) });
+    } catch (error) {
+        next(error);
+    }
+};
+export const getMessages = async (req, res, next) => {};
+export const getMessage = async (req, res, next) => {};
+export const removeMessage = async (req, res, next) => {};
+export const updateMessage = async (req, res, next) => {};
--- src/models/messageModel.js
+++ src/models/messageModel.js
@@ -6,16 +6,6 @@ const MessageSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
-    link: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    img: {
-        type: String,
-        required: true,
-        trim: true
-    },
     sm: [{
         type: String,
     }],
