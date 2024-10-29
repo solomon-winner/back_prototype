fd4b4efacfeb173f685c56b700eb2c96e7d6f444
solomon-winner <solyalew3@gmail.com>
2024-10-29 13:38:25 +0300

    ...email helper file...

---
+++ src/helpers/emailHelper.js
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer';
+
+export const EmailHelper = () => {
+    const sendEmail = async (email, subject, message) => {
+        try {
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.PASSWORD,
+                },
+            });
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: email,
+                subject,
+                text: message,
+            };
+            await transporter.sendMail(mailOptions);
+        } catch (error) {
+            throw new Error(error);
+        }
+    };
+    return {
+        sendEmail,
+    };
+}
