f7f133720e9cfaea1e6aed82bbfb2f158d76ad03
solomon-winner <solyalew3@gmail.com>
2024-11-02 15:21:59 +0300

    ...validation...

--- src/middlewares/validateGeneral.js
+++ src/middlewares/validateGeneral.js
@@ -2,13 +2,6 @@ import { validate } from "express-validation";
 import Joi from "joi";
 import mongoose from "mongoose";
 
-const objectId = Joi.string().custom((value, helpers) => {
-  if (!mongoose.Types.ObjectId.isValid(value)) {
-    return helpers.message("Invalid MongoDB ID");
-  }
-  return value;
-}, "MongoDB ObjectId Validation");
-
 const generalSchema = {
   body: Joi.object({
     bannerPic: Joi.string().required().trim().messages({
