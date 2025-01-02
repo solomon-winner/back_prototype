de7a99acedb847fea8f5fca6ab1c7e55d6d2b3eb
solomon-winner <solyalew3@gmail.com>
2025-01-02 11:35:56 +0300

    ...upload...

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,40 +1,29 @@
-import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url";
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../../public/geojson"));
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
-    );
-  },
-});
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 },
-  fileFilter: (req, file, cb) => {
-    checkFileType(file, cb);
-  },
-}).single("geoJson");
+const storage = multer.memoryStorage();
 
 function checkFileType(file, cb) {
-  const filetypes = /geojson/;
-  const mimetypes = /application\/geo\+json || application\/octet-stream/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = mimetypes.test(file.mimetype);
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: images Only!");
-  }
+    const filetypes = /png|jpg|jpeg/;
+    const mimetypes = /image\/png|image\/jpeg/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = mimetypes.test(file.mimetype);
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Only png, jpg, jpeg files are allowed!');
+    }
 }
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb);
+    },
+});
 
 export default upload;
