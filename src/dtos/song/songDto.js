8d444b6abe7fc70b9c1fb8e8815e8f6de70670ec
solomon-winner <solyalew3@gmail.com>
2025-02-23 17:20:51 +0300

    ...data transfer object...

--- src/dtos/song/songDto.js
+++ src/dtos/song/songDto.js
@@ -1,8 +1,8 @@
 export class SongDTO {
-  constructor({_id, title,youtube_link, spotifyLink, appleMusicLink, amazonLink, img, albums, type, createdAt, updatedAt}) {
+  constructor({_id, title,youtubeLink, spotifyLink, appleMusicLink, amazonLink, img, albums, type, createdAt, updatedAt}) {
     this.id = _id;
     this.title = title;
-    this.youtube_link = youtube_link;
+    this.youtubeLink = youtubeLink;
     this.spotifyLink = spotifyLink;
     this.appleMusicLink = appleMusicLink;
     this.amazonLink = amazonLink;
